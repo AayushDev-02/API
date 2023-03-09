a9a2315654ec82aa664b5db3723141572b10a84f
Aayush Yadav <yadavaayush002@gmail.com>
2023-03-09 21:42:26 +0530

    made exress js connection

---
+++ WeatherProject/app.js
@@ -0,0 +1,12 @@
+const express= require("express")
+
+const app = express();
+const port = 3000;
+
+app.get('/' , (req,res) =>{
+    res.send("Server is running");
+})
+
+app.listen(port, () =>{
+    console.log("Server running at post" + port);
+})
